f65205007c727fb4ad62acb1195f92000c1e74c3
SirIsaacNeutron <dbadir@cox.net>
2023-01-20 02:10:41 -0800

    Add player and gameboard factories

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// X player
+let player1 = null
+let player2 = null
+
+let gameboard = null
+
 const twoPlayersButton = document.querySelector(".two-players-btn")
 
 twoPlayersButton.addEventListener("click", createPlayerNamesForm)
@@ -42,5 +48,99 @@ function createPlayerNamesForm() {
     submitButton.classList.add("submit-btn")
     nameForm.appendChild(submitButton)
 
+    nameForm.addEventListener("submit", handleSubmission)
+
     buttonsSection.replaceWith(nameForm)
+}
+
+function handleSubmission(e) {
+    e.preventDefault()
+
+    const player1Name = document.getElementById("xPlayerInput").value
+    const player2Name = document.getElementById("oPlayerInput").value
+
+    player1 = playerFactory(player1Name, "X")
+    player2 = playerFactory(player2Name, "O")
+
+    gameboard = gameboardFactory([player1, player2])
+}
+
+function playerFactory(playerName, playerMarker) {
+    let name = playerName
+    const marker = playerMarker
+
+    function getMarker() {
+        return marker
+    }
+
+    function getName() {
+        return name
+    }
+
+    function setName(newName) {
+        name = newName
+    }
+
+    return { setName, getName, getMarker }
+}
+
+function gameboardFactory(playersArray) {
+    const xPlayer = playersArray[0]
+    const yPlayer = playersArray[1]
+    
+    let currentPlayer = xPlayer
+    const EMPTY_SPACE = ""
+    
+    const board = createBoard()
+
+    function createBoard() {
+        const newBoard = []
+        for (let i = 0; i < 2; ++i) {
+            const newRow = [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE]
+            newBoard.push(newRow)
+        }
+        return newBoard
+    }
+
+    function getBoard() {
+        return board
+    }
+
+    function hasGameBeenWon() {
+        if (board[0][0] === board[0][1] && board[0][1] === board[0][2]) { return true }
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) { return true }
+        if (board[0][0] === board[1][0] && board[1][0] === board[2][0]) { return true }
+        if (board[0][2] === board[1][2] && board[1][2] === board[2][2]) { return true }
+        if (board[2][0] === board[2][1] && board[2][1] === board[2][2]) { return true }
+        return false
+    }
+
+    function makeMove(row, col) {
+        if (!isCellOccupied(row, col)) {
+            board[row][col] = currentPlayer.getMarker()
+
+            if (currentPlayer === xPlayer) {
+                currentPlayer = yPlayer
+            }
+            else {
+                currentPlayer = xPlayer
+            }
+        }
+    }
+
+    function isCellOccupied(row, col) {
+        return board[row][col] !== EMPTY_SPACE
+    }
+
+    function isGameTied() {
+        for (let i = 0; i < 2; ++i) {
+            const currentRow = board[0]
+            if (currentRow.includes(EMPTY_SPACE)) {
+                return false
+            }
+        }
+        return true
+    }
+
+    return { getBoard, hasGameBeenWon, makeMove, isCellOccupied, isGameTied }
 }
